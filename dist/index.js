cff9bf3e7f3d9aa684bc7d21385dcbd91120abbf
Yashiro <51014425+yuhui1208@users.noreply.github.com>
2021-09-29 14:42:01 +0800

    Add logs (#4)

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,9 @@ async function main() {
     const cid = core.getInput('cid'); // Currently, we only support CIDv0
     const chainAddr = core.getInput('crust-endpoint');
 
+    console.log('cid', cid)
+    console.log('chainAddr', chainAddr)
+
     // 2. Check cid
     if (!checkCid(cid)) {
         throw new Error('Illegal inputs');
@@ -30,12 +33,17 @@ async function main() {
 
     const file = parsObj(await chain.query.market.files(cid));
 
+    console.log('file', file)
+
     if (file) {
+        console.log('reported_replica_count', file.reported_replica_count)
         core.setOutput('replicaCount', file.reported_replica_count);
     } else {
+        console.log('File not found or no replicas')
         core.setOutput('replicaCount', 0);
     }
- 
+
+    await chain.disconnect();
 }
 
 main().catch(error => {
